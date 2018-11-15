4f6102bc6d47c461e3ce892ee1e9db2925997d78
helenahwang <yeonha.hwang@gmail.com>
2018-11-15 11:25:19 +0900

    Initial Commit

---
+++ UseCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UseCoreData
+//
+//  Created by 503-02 on 15/11/2018.
+//  Copyright © 2018 YEONHA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
